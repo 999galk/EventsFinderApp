a3b8c9083e866bbfb38d088cccb20d7cd56e4bd1
Gal Kol <999galk@gmail.com>
2020-02-03 16:52:03 +0200

    Updates

---
+++ precache-manifest.6a64c3d701f17ac6c1bf0609ef3b0555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a70b821a4037ba41256",
+    "url": "/999galk/EventsFinderApp/static/css/main.04db37ce.chunk.css"
+  },
+  {
+    "revision": "6a70b821a4037ba41256",
+    "url": "/999galk/EventsFinderApp/static/js/main.79101198.chunk.js"
+  },
+  {
+    "revision": "c6dcfa2161fa13682107",
+    "url": "/999galk/EventsFinderApp/static/js/runtime~main.1ed4940a.js"
+  },
+  {
+    "revision": "99d69db08aff2e98a21a",
+    "url": "/999galk/EventsFinderApp/static/css/2.39d784f4.chunk.css"
+  },
+  {
+    "revision": "99d69db08aff2e98a21a",
+    "url": "/999galk/EventsFinderApp/static/js/2.1c2705ca.chunk.js"
+  },
+  {
+    "revision": "0f40f4737fd2c08cb56a9ec7e973154b",
+    "url": "/999galk/EventsFinderApp/static/media/headerImg.0f40f473.jpg"
+  },
+  {
+    "revision": "6bf3a8babdba29ec873b3ddc3e3c6214",
+    "url": "/999galk/EventsFinderApp/index.html"
+  }
+];
